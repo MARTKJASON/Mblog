dbc8f379159d0508d9d98cff495eb7eec3a8a5cb
Mjdelima20 <markjason.delima@sun-asterisk.com>
2024-08-28 12:39:24 +0800

    Update Vite configuration to output to public/build

--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,4 @@ export default defineConfig({
         }),
         react(),
     ],
-    build: {
-        outDir: 'public/build',  // Specify the output directory for the build
-        manifest: true,  // Ensure Vite generates a manifest.json file
-        rollupOptions: {
-            output: {
-                entryFileNames: 'js/[name].js',  // Customize entry file naming if needed
-                chunkFileNames: 'js/[name].js',   // Customize chunk file naming if needed
-                assetFileNames: 'assets/[name].[ext]', // Customize asset file naming if needed
-            }
-        }
-    }
 });
